7c770490ebafc29341424bb2320755637090f0a3
trentleslie <trentleslie@gmail.com>
2013-04-26 12:04:35 -0400

    update bowtie2 shell scripts

--- bowtie2/bowtie2-077.sh
+++ bowtie2/bowtie2-077.sh
@@ -2,7 +2,7 @@
 #PBS -l walltime=12:00:00
 #PBS -l nodes=2:ppn=8
 
-cd $PBS_O_WORKDIR
+cd $TMPDIR
 PATH=$PATH":/nfs/09/ucn1113/samtools-0.1.18/:/nfs/09/ucn1113/bowtie2-2.1.0/:/nfs/09/ucn1113/HTSeq-0.5.4p1/build/scripts-2.7/"
 
 
@@ -15,6 +15,6 @@ cp /nfs/09/ucn1113/bowtie2/purpurea.4.bt2 .
 cp /nfs/09/ucn1113/bowtie2/purpurea.rev.1.bt2 .
 cp /nfs/09/ucn1113/bowtie2/purpurea.rev.2.bt2 .
 
-bowtie2 -t -I 150 -p 4 -1 002735_077-1-2_AGTCAA_R1_filtered.fastq -2 002735_077-1-2_AGTCAA_R2_filtered.fastq purpurea -S 077-1-2.sam
+bowtie2 -t -I 150 -X 350 --no-discordant -p 4 -1 002735_077-1-2_AGTCAA_R1_filtered.fastq -2 002735_077-1-2_AGTCAA_R2_filtered.fastq purpurea -S 077-1-2.sam
 
 cp 077-1-2.sam /nfs/09/ucn1113/bowtie2_output/
--- bowtie2/bowtie2-336.sh
+++ bowtie2/bowtie2-336.sh
@@ -1,7 +1,6 @@
 #PBS -N bowtie2_336
 #PBS -l walltime=12:00:00
 #PBS -l nodes=2:ppn=8
-cd $PBS_O_WORKDIR
 
 cd $TMPDIR
 
@@ -16,6 +15,6 @@ cp /nfs/09/ucn1113/bowtie2/purpurea.4.bt2 .
 cp /nfs/09/ucn1113/bowtie2/purpurea.rev.1.bt2 .
 cp /nfs/09/ucn1113/bowtie2/purpurea.rev.2.bt2 .
 
-bowtie2 -t -I 150 -p 4 -1 002737_336-1-2_ATGTCA_R1_filtered.fastq -2 002737_336-1-2_ATGTCA_R2_filtered.fastq purpurea -S 336-1-2.sam
+bowtie2 -t -I 150 -X 350 --no-discordant -p 4 -1 002737_336-1-2_ATGTCA_R1_filtered.fastq -2 002737_336-1-2_ATGTCA_R2_filtered.fastq purpurea -S 336-1-2.sam
 
 cp 336-1-2.sam /nfs/09/ucn1113/bowtie2_output/
